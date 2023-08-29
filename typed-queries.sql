a9fe5c3711f10fa0d433ca9076b0b04717d406de
Grace Bethany Hoober <grace.hoober66@gmail.com>
2023-08-29 16:35:49 -0700

    finished first exercise

--- typed-queries.sql
+++ typed-queries.sql
@@ -1,6 +1,25 @@
-vehicles=# SELECT first_name, COUNT(*)
+SELECT first_name, COUNT(*)
 FROM owners
 JOIN vehicles
 ON owners.id = vehicles.owner_id
-GROUP BY first_name
+GROUP BY owners.id;
+
+SELECT *
+FROM owners
+LEFT JOIN vehicles
+ON owners.id = vehicles.owner_id;
+
+SELECT *
+FROM owners
+FULL OUTER JOIN vehicles
+ON owners.id = vehicles.owner_id;
+
+SELECT first_name, ROUND(AVG(price),0) AS AVERAGE_PRICE, COUNT(*)
+FROM owners
+JOIN vehicles
+ON owners.id = vehicles.owner_id
+GROUP BY owners.id
+HAVING AVG(price) > 10000
+ORDER BY first_name DESC;
+
 
